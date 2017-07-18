7ae3d98e00e1a964dd0edba72050b42787b1ffa3
Yi Lu <yilu@Yis-iMac.local>
2017-07-18 14:30:49 -0400

    customer templates finished, nav customer links added, h1 tag added, responsive-table added

--- dist/assets/_theme.js
+++ dist/assets/_theme.js
@@ -3254,20 +3254,29 @@ y=function(){x();return l()},H=function(){G=!0;f.off("touchmove",l);f.off("scrol
 
 $(document).ready(function() {
 
+/* =========================================================================
+    Wait For Image
+========================================================================= */
 
 
  $('.js-wait-for-image-wrapper').waitForImages(function() {
-        $('.hidden-before-images-ready').animate({
-            opacity: '1'
-        }, 350);
+    $('.hidden-before-images-ready').animate({
+        opacity: '1'
+    }, 350);
  });
 
- // VIDEO
+
+/* =========================================================================
+    Fitvids
+========================================================================= */
+
 
  $('.fitvids').fitVids();
 
- // BROWSER SCROLLED DETECT
 
+/* =========================================================================
+    Browser Scrolled Detect
+========================================================================= */
 
  function scrolledCheck() {
 
@@ -3292,8 +3301,9 @@ $(document).ready(function() {
  });
 
 
-
- // 100vh BACKUP FUNCTION
+/* =========================================================================
+    100vh Backup
+========================================================================= */
 
  function setHeight100vh() {
         $('.height-100vh').height($(window).height());
@@ -3303,107 +3313,129 @@ $(document).ready(function() {
         $(window).resize($.debounce(250, setHeight100vh));
  }
 
- //SEARCH
+ /* =========================================================================
+    Search
+========================================================================= */
+
  $('.desktop-navbar-search').hover(
-        function() {
-             $(this).addClass('open');
-        },
-        function() {
-             $(this).removeClass('open');
-        });
+    function() {
+         $(this).addClass('open');
+    },
+    function() {
+         $(this).removeClass('open');
+    });
 
-
- //DROPDOWN
+/* =========================================================================
+    Dropdown
+========================================================================= */
 
  if ($('html.mod-no-mobile').length) {
 
-        $('.js-dropdown-hover').hover(
-             function() {
-                    $(this).addClass('open');
-             },
-             function() {
-                    $(this).removeClass('open');
-             });
+    $('.js-dropdown-hover').hover(
+         function() {
+                $(this).addClass('open');
+         },
+         function() {
+                $(this).removeClass('open');
+         });
 
  }
 
  if ($('html.mod-mobile').length) {
 
-        $('.js-dropdown-toggle').click(function() {
+    $('.js-dropdown-toggle').click(function() {
 
-             var $dropdown = $(this).parent();
+         var $dropdown = $(this).parent();
 
-             $dropdown.toggleClass('open')
-                    .siblings('.dropdown').removeClass('open');
+         $dropdown.toggleClass('open')
+                .siblings('.dropdown').removeClass('open');
 
-        });
+    });
 
  }
 
 
+/* =========================================================================
+    Body Class
+========================================================================= */
 
- // BODY CLASS
 
  $(document).on('click', '.js-body-add-class', function() {
-        $('body').addClass($(this).data('body-class'));
+    $('body').addClass($(this).data('body-class'));
  });
 
  $(document).on('click', '.js-body-remove-class', function() {
-        $('body').removeClass($(this).data('body-class'));
+    $('body').removeClass($(this).data('body-class'));
  });
 
  $(document).on('click', '.js-body-toggle-class', function() {
-        $('body').toggleClass($(this).data('body-class'));
+    $('body').toggleClass($(this).data('body-class'));
  });
 
- // ELEMENT FADEIN FADEOUT
+
+/* =========================================================================
+    Element Fadein Fadeout
+========================================================================= */
+
 
  $(document).on('click', '.js-element-fadein', function() {
-        $($(this).data('element-selector')).fadeIn();
+    $($(this).data('element-selector')).fadeIn();
  });
+
  $(document).on('click', '.js-element-fadeout', function() {
-        $($(this).data('element-selector')).fadeOut();
+    $($(this).data('element-selector')).fadeOut();
  });
+
  $(document).on('click', '.js-element-toggle', function() {
-        $($(this).data('element-selector')).fadeToggle(300);
+    $($(this).data('element-selector')).fadeToggle(300);
  });
 
 
- // STOP PROPAGATION
+/* =========================================================================
+    Stop Propagation
+========================================================================= */
+
 
  $(document).on('click', '.js-stop-propagation', function(event) {
-        event.stopPropagation();
+    event.stopPropagation();
  });
 
 
- // SCROLL TO ELEMENT
+/* =========================================================================
+    Scroll To Element
+========================================================================= */
+
 
  $(document).on('click', '.js-body-scroll-to-element', function() {
-        var elementSelector = $(this).data('element-selector');
-        var scrollOffset = $(this).data('scroll-offset');
-        $('html, body').animate({
-             scrollTop: $(elementSelector).offset().top - scrollOffset
-        }, 600, 'easeOutExpo');
+    var elementSelector = $(this).data('element-selector');
+    var scrollOffset = $(this).data('scroll-offset');
+    $('html, body').animate({
+         scrollTop: $(elementSelector).offset().top - scrollOffset
+    }, 600, 'easeOutExpo');
  });
 
 
+/* =========================================================================
+    Image Swap
+========================================================================= */
 
- // IMAGE SWAP
 
  var imageSourceSwap = function() {
 
-        var $this = $(this);
-        var swapImageSrc = $this.attr('data-swap-image-src');
-        $this.attr('data-swap-image-src', $this.attr('src'));
-        $this.attr('src', swapImageSrc);
+    var $this = $(this);
+    var swapImageSrc = $this.attr('data-swap-image-src');
+    $this.attr('data-swap-image-src', $this.attr('src'));
+    $this.attr('src', swapImageSrc);
 
  };
 
  $('.js-product-image-swap').hover(imageSourceSwap, imageSourceSwap);
 
 
+/* =========================================================================
+    Pagination Load More Ajax
+========================================================================= */
 
-//PAGINATION LOAD MORE AJAX
 
 var paginationIsLoading = false;
 
@@ -3458,41 +3490,77 @@ $(document).on('click', '.js-pagination-load-more', function(event) {
 
 
 
+/* =========================================================================
+    Product Image Fullscreen
+========================================================================= */
 
-    // PRODUCT IMAGE FULLSCREEN
 
-    $(document).on('click', '.js-product-image-fullscreen', function() {
-        $('.product-image-popup').fadeIn().find('img').prop('src', $(this).find('img').prop('src'));
+$(document).on('click', '.js-product-image-fullscreen', function() {
+    $('.product-image-popup').fadeIn().find('img').prop('src', $(this).find('img').prop('src'));
+});
+
+
+/* =========================================================================
+    Email Subscribe Popup
+========================================================================= */
+
+if (!$.cookie('site_visited')) {
+    $('.subscribe-popup').fadeIn();
+    $.cookie('site_visited', 'true', {
+         expires: 7,
+         path: '/'
     });
+}
 
 
-    // EMAIL SUBSCRIBE POPUP
+/* =========================================================================
+    Customer
+========================================================================= */
 
-    if (!$.cookie('site_visited')) {
-        $('.subscribe-popup').fadeIn();
-        $.cookie('site_visited', 'true', {
-             expires: 7,
-             path: '/'
-        });
+
+$(document).on('click', '.js-rest-password-fadein', function() {
+
+    $('.recover-password-form').fadeIn(0);
+    $('.customer-login-form').fadeOut(0);
+
+});
+
+$(document).on('click', '.js-rest-password-fadeout', function() {
+
+    $('.recover-password-form').fadeOut(0);
+    $('.customer-login-form').fadeIn(0);
+
+});
+
+
+
+// Initialize observers on address selectors, defined in shopify_common.js
+if (Shopify) {
+    new Shopify.CountryProvinceSelector('AddressCountryNew', 'AddressProvinceNew', {
+      hideElement: 'AddressProvinceContainerNew'
+    });
+}
+
+// Initialize each edit form's country/province selector
+$('.address-country-option').each(function() {
+    var formId = $(this).data('form-id');
+    var countrySelector = 'AddressCountry_' + formId;
+    var provinceSelector = 'AddressProvince_' + formId;
+    var containerSelector = 'AddressProvinceContainer_' + formId;
+
+    new Shopify.CountryProvinceSelector(countrySelector, provinceSelector, {
+        hideElement: containerSelector
+    });
+});
+
+$('.address-delete').on('click', function() {
+    var $el = $(this);
+    var formId = $el.data('form-id');
+    var confirmMessage = $el.data('confirm-message');
+    if (confirm(confirmMessage || 'Are you sure you wish to delete this address?')) {
+        Shopify.postLink('/account/addresses/' + formId, {parameters: {_method: 'delete'}});
     }
-
-
-
-     // Customer
-
-    $(document).on('click', '.js-rest-password-fadein', function() {
-
-        $('.recover-password-form').fadeIn(0);
-        $('.customer-login-form').fadeOut(0);
-
-    });
-
-    $(document).on('click', '.js-rest-password-fadeout', function() {
-
-        $('.recover-password-form').fadeOut(0);
-        $('.customer-login-form').fadeIn(0);
-
-    });
+});
 
 
 
@@ -3507,28 +3575,23 @@ $(document).on('click', '.js-pagination-load-more', function(event) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// HOMEPAGE PAGE
+/* =========================================================================
+    Homepage Page
+========================================================================= */
 
 if ($('.template-index').length) {
 
 
 }
 
-// END HOMEPAGE PAGE
 
 
-// COLLECTION PAGE
+
+
+
+/* =========================================================================
+    Collection Page
+========================================================================= */
 
 
 if ($('.template-collection').length) {
@@ -3543,13 +3606,11 @@ if ($('.template-collection').length) {
 
 }
 
-// END COLLECTION PAGE
 
+/* =========================================================================
+    Product Page
+========================================================================= */
 
-
-
-
-// PRODUCT PAGE
 
 if ($('.template-product').length) {
 
@@ -3608,12 +3669,6 @@ if ($('.template-product').length) {
 
 }
 
-
-
-
-
-
-// END PRODUCT PAGE
 
 
 
